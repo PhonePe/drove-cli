97ece86beed6492acc17ae54a1216abab7702e68
Tushar Naik <tushar.knaik@gmail.com>
2024-02-01 00:40:05 +0530

    Added a check on api_response in droveclient

--- plugins/applications.py
+++ plugins/applications.py
@@ -214,8 +214,8 @@ class Applications(plugins.DrovePlugin):
             print("Application restart command accepted.")
 
     def cancel_app_operation(self, options: SimpleNamespace):
-        self.drove_client.post("/apis/v1/operations/{appId}/cancel".format(appId=options.app_id), None, False)
-        print("Operation cancellation request registered.")
+        data = self.drove_client.post("/apis/v1/operations/{appId}/cancel".format(appId=options.app_id), None, False)
+        print("Operation cancellation request registered :" + data["message"])
 
     @retry(wait=tenacity.wait_exponential(multiplier=1, min=4, max=10),
            retry=tenacity.retry_if_result(lambda x: x == False))
